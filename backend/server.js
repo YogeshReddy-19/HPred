cf8b240eb62b43e529fa2e9e7efdb8f16f50d95b
Yogesh Reddy Baddala <yogeshreddybaddala@Yogeshs-MacBook-Air.local>
2026-05-21 19:17:03 +0530

    fix render port

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import profileRoutes from "./routes/profileRoute.js"
 import "dotenv/config";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors({
     origin : true,
